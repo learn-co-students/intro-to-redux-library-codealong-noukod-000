8030fc886375dc6e1db523315b18a11b447e32b8
eTarget1 <louisclifford10@yahoo.fr>
2019-05-01 13:16:06 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -32,4 +32,7 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+  )(App);
